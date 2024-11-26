cc3d14db205817b646ed11cc73db06b6644ffb10
TengNapadon <napadonnantasuk@gmail.com>
2024-11-26 15:54:45 +0700

    Add validator in userModel

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -12,12 +12,20 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
         trim: true,
-        lowercase: true
+        lowercase: true,
+        match: [/\S+@\S+\.\S+/, 'is invalid']
     },
     password: {
         type: String,
         required: true,
-        minlength: 6
+        minlength: 6,
+        validate: {
+            validator: function(value) {
+                const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/;
+                return passwordRegex.test(value);
+            },
+            message: 'Password must contain at least one lowercase letter, one uppercase letter, and one number'
+        }
     }
 }, {
     timestamps: true
