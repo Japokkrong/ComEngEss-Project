81e95bd4a22d026c03e4ae751bbb26f977cef9d5
TengNapadon <napadonnantasuk@gmail.com>
2024-11-28 00:16:21 +0700

    Seperate error message for register in Backend

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -9,7 +9,19 @@ export const createUser = async (req, res) => {
     }
     catch(err) {
         if (err.name === "ValidationError") {
-            res.status(400).json({ error: "Email or username is already registered" });
+            if (err.errors.email) {
+                if (err.errors.email.kind === "unique") {
+                    res.status(400).json({ error: "Email is already registered" });
+                } else {
+                    res.status(400).json({ error: "Invalid email format" });
+                }
+            } else if (err.errors.username) {
+                res.status(400).json({ error: "Username is already registered" });
+            } else if (err.errors.password) {
+                res.status(400).json({ error: err.errors.password.message });
+            } else {
+                res.status(400).json({ error: "Validation error" });
+            }
         } else {
             res.status(500).json({ error: "Internal server error." });
         }
